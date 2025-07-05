c56a271c919c6ce77a8ce607b015b7ddcda1241e
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-07-05 16:11:54 -0400

    Update Main.java to initialize Scanner and launch CLI menu

--- src/main/java/com/jmill29/tvtrackerfrontend/ui/Main.java
+++ src/main/java/com/jmill29/tvtrackerfrontend/ui/Main.java
@@ -1,8 +1,12 @@
 package com.jmill29.tvtrackerfrontend.ui;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the TV Tracker Console App!");
-        // Future: display login/register menu
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu(scanner);
+        menu.displayMenu();
+        scanner.close();
     }
 }
