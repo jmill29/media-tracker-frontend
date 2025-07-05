178fd3423bfb3c7be58df3f2b5b0a707dd557b8e
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-07-05 18:33:34 -0400

    Add LocalDateTimeAdapter for Gson serialization/deserialization support
    - Implements custom TypeAdapter for converting LocalDateTime to ISO-8601 string format and vice versa
    - Enables compatibility between Gson and Java's LocalDateTime during JSON parsing

--- src/main/java/com/jmill29/tvtrackerfrontend/util/LocalDateTimeAdapter.java
+++ src/main/java/com/jmill29/tvtrackerfrontend/util/LocalDateTimeAdapter.java
@@ -1,2 +1,24 @@
-package com.jmill29.tvtrackerfrontend.util;public class LocalDateTimeAdapter {
+package com.jmill29.tvtrackerfrontend.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    @Override
+    public void write(JsonWriter out, LocalDateTime value) throws IOException {
+        out.value(value.format(formatter));
+    }
+
+    @Override
+    public LocalDateTime read(JsonReader in) throws IOException {
+        return LocalDateTime.parse(in.nextString(), formatter);
+    }
 }
