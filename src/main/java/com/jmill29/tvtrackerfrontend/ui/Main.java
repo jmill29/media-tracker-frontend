1bf03dc5b03e0fa185fa064dd4ca608d9ecb0d95
Jacob Miller <jacoblmiller.jlm@gmail.com>
2025-07-05 10:52:38 -0400

    Initial commit

---
+++ src/main/java/com/jmill29/tvtrackerfrontend/ui/Main.java
@@ -0,0 +1,8 @@
+package com.jmill29.tvtrackerfrontend.ui;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the TV Tracker Console App!");
+        // Future: display login/register menu
+    }
+}
